5a86fef92d532a1cc7b6f2986c6fe537088e9828
Keerthana <keerthana@gmail.com>
2026-08-20 20:45:01 +0530

    Updated database connection for Aiven

--- database/db.py
+++ database/db.py
@@ -1,13 +1,15 @@
 import mysql.connector
+import streamlit as st
+
+
 def get_connection():
     connection = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="Keerthuma31",
-    database="ai_skill_gap_analyzer"
-)
+        host=st.secrets["DB_HOST"],
+        port=int(st.secrets["DB_PORT"]),
+        user=st.secrets["DB_USER"],
+        password=st.secrets["DB_PASSWORD"],
+        database=st.secrets["DB_NAME"],
+        ssl_ca=st.secrets["DB_SSL_CA"]
+    )
+
     return connection
-
-
-
-
